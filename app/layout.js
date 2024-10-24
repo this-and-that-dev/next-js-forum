79913957cd7bea26ebbe3a8c50146fe6350dbf74
공수한 <ksh92315@pionnet.co.kr>
2024-10-24 23:54:46 +0900

    글 삭제 할때 애니메이션 추가(서서히 사라짐)

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({children}) {
             <div className="navbar">
                 <Link href="/" className="logo">Appleforum</Link>
                 <Link href="/list">List</Link>
+                <Link href="/write">write</Link>
             </div>
         {children}
         </body>
