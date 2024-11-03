71ad16e3eb4ca2c5fa3de432764c521b6034d2d6
공수한 <ksh92315@pionnet.co.kr>
2024-11-03 16:14:19 +0900

    댓글 조회 기능 추가

--- app/detail/Comment.js
+++ app/detail/Comment.js
@@ -1,27 +1,41 @@
 'use client';
 
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 export default function Comment({contentId}) {
 
     let [comment, setComment] = useState('');
+    let [comments, setComments] = useState([]);
 
-    return (
-        <div>
+    useEffect(() => {
+        axios.get('/api/comment?contentId=' + contentId)
+            .then((result) => {
+                setComments(result.data);
+            })
+    }, []);
+
+    return (<div>
             <div>댓글 목록 보여줄 부분</div>
+            {
+                comments.length > 0 ?
+                    comments.map((comment, index) => {
+                        return <div key={index}>{comment.comment}</div>;
+                    }) : null
+            }
             <input type={"text"} onChange={(e) => {
                 setComment(e.target.value);
             }}/>
             <button onClick={() => {
-                axios.post("/api/comment", {comment : comment, contentId : contentId})
+                axios.post("/api/comment", {comment: comment, contentId: contentId})
                     .then(result => {
                         console.log(result);
                     })
                     .catch(e => {
                         console.log(e);
                     })
-            }}>댓글 전송</button>
+            }}>댓글 전송
+            </button>
         </div>
     )
 };
--- pages/api/comment.js
+++ pages/api/comment.js
@@ -4,17 +4,14 @@ import {connectDB} from "@/util/database";
 import {ObjectId} from "mongodb";
 
 export default async function handler(request, response) {
-    let session = await getServerSession(request, response, authOptions);
 
-    if (session === null) {
-        response.status(401).json("로그인하셈");
-    }
+    if (request.method === "POST") {
+        let session = await getServerSession(request, response, authOptions);
 
-    if (request.method !== "POST") {
-        response.status(405).json("post 만 가능");
-    }
+        if (session === null) {
+            response.status(401).json("로그인하셈");
+        }
 
-    if (session != null && request.method === "POST") {
         let commentBody = {
             comment: request.body.comment,
             contentId: new ObjectId(request.body.contentId),
@@ -29,5 +26,12 @@ export default async function handler(request, response) {
             console.log(e);
             response.status(500).json("댓글 등록하다 에러남");
         }
+    }
+    if (request.method === "GET") {
+        console.log(request.query.contentId);
+        const db = (await connectDB).db("forum");
+        let result = await db.collection('comment').find({contentId: new ObjectId(request.query.contentId)}).toArray();
+        console.log(result)
+        response.status(200).json(result);
     }
 }
