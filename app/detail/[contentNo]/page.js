3b0e713ae71885c271716446035a7fe721dd9954
공수한 <ksh92315@pionnet.co.kr>
2024-11-11 00:20:54 +0900

    게시글 상세에 이미지있으면 이미지 노출 처리

--- app/detail/[contentNo]/page.js
+++ app/detail/[contentNo]/page.js
@@ -16,6 +16,9 @@ export default async function Detail(props) {
             <h4>상세페이지</h4>
             <h4>{result.title}</h4>
             <p>{result.content}</p>
+            {
+                result.imgUrl !== undefined && <img src={result.imgUrl} alt={result.title} />
+            }
             <Comment contentId={props.params.contentNo}/>
         </div>
     );
